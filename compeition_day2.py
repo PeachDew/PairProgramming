728ef5acbeb839a3721caf6cedffee8563442c2f
PeachDew <chin.furui.raymond@dhs.sg>
2016-04-19 12:01:09 +0800

    testing

---
+++ compeition_day2.py
@@ -0,0 +1,46 @@
+# Qn 1: Find Duplicates
+
+def findDuplicate(array):
+    numbers = list(set(array))
+    for i in numbers:
+        array.remove(i)
+    print(len(array))
+findDuplicate([1,2,3,4,5,5,9,3,5,3,1])
+
+# Qn
+
+
+# Qn
+
+
+# Qn
+
+
+# Qn
+
+
+# Qn
+
+
+# Qn
+
+
+# Qn
+
+
+# Qn
+
+
+# Qn
+
+
+# Qn
+
+
+# Qn
+
+
+# Qn
+
+
+
